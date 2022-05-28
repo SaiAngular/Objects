ac8c4c824862489fb74c80e0d260ec27b228e3ed
saikirangit config --global user.name saikirangit config --global user.name "saikirangit config --global user.name "saikiran"git config --global user.name "saikirangit config --global user.name "saikiran <pillellasai@gmail.com>
2022-05-28 17:17:00 +0530

    adding opeartors

---
+++ opt.js
@@ -0,0 +1,38 @@
+// Program for operators
+
+var a=1+2;
+var b= 3*4;
+console.log(a);
+console.log(b);
+
+var c=10;
+var d=2;
+var result=c/d;
+
+console.log(result);
+
+// Paasing arguments through functions 
+
+function  add(e,f)
+{
+    var sum=e+f;
+    
+    console.log(sum);
+}
+add(5,5);
+
+// Program for multiple operators in single expression 
+
+var g =5;
+var h =7;
+var i=6;
+var line=g+(h-i);
+console.log(line);
+
+// Program for Associativity in operators.
+
+var j=3;
+var k=5;
+var l=7;
+var m= j=k=l;
+console.log(m);
