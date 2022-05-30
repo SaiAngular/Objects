f91a93d88a982c19f75fb24a2765ee8bd9f90e50
saikirangit config --global user.name saikirangit config --global user.name "saikirangit config --global user.name "saikiran"git config --global user.name "saikirangit config --global user.name "saikiran <pillellasai@gmail.com>
2022-05-30 06:59:01 +0530

    functions code

--- funct.js
+++ funct.js
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html>
-  <head>
-      <body>
-
-     
-    <title>Training</title>
-    <h1> Hello People </h1>
-    <script src="funct.js"></script>
-</body>
-
-</html>
+var a="sai";
+var b="kiran";
+ var c= a + b;
+ console.log(c"   ");
+ 
