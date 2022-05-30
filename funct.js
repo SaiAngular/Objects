ae4f62d1d475a888b9afc04473603ee9b750104b
saikirangit config --global user.name saikirangit config --global user.name "saikirangit config --global user.name "saikiran"git config --global user.name "saikirangit config --global user.name "saikiran <pillellasai@gmail.com>
2022-05-30 07:04:23 +0530

    function argument

--- funct.js
+++ funct.js
@@ -1,5 +1,19 @@
+// Program to COncatinate 2 varaiables
 var a="sai";
 var b="kiran";
  var c= a + b;
- console.log(c"   ");
+ console.log(c);
+
+
+ // Program for 2 varaibales to concatinate through functions 
+
+ function name1(firstname, last name) {
+     var fullname=firstname+lastname
+     return fullname;
+ }
+
+ var name=name1(sai,kiran);
+
+ console.log(full name);
+
  
